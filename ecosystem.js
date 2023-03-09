57e85d1075f75ac912524539db52d31976ae794c
EduardoReolon <eduardo_reolon@hotmail.com>
2023-03-09 12:57:02 -0300

    ecosystem

--- ecosystem.js
+++ ecosystem.js
@@ -2,7 +2,7 @@
     apps: [{
         "name": "micro",
         "script": "build/index.js",// name of the startup file
-        "exec_mode": "cluster",  // to turn on cluster mode; defaults to 'fork' mode
+        "exec_mode": "fork",  // to turn on cluster mode; defaults to 'fork' mode
         // "instances": 4,          // number of workers you want to run
         // "env": {
         //     "PORT": "9090" // the port on which the app should listen
