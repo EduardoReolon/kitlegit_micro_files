36976c81a98c5e221465ee12e7e18dfacabb2661
EduardoReolon <eduardo_reolon@hotmail.com>
2023-04-09 18:49:44 -0300

    ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,12 +4,12 @@ module.exports = {
         script: "build/index.js",// name of the startup file
         exec_mode: "cluster",  // to turn on cluster mode; defaults to 'fork' mode
         max_memory_restart: "250M",
-        kill_timeout : 5000, // 10 minutes
+        kill_timeout : 30000, // 30 segundos
         // env           : { 'LD_PRELOAD': '/usr/lib/x86_64-linux-gnu/libjemalloc.so.1', 'NODE_ENV': 'production' },
         // env_production: { 'LD_PRELOAD': '/usr/lib/x86_64-linux-gnu/libjemalloc.so.1', 'NODE_ENV': 'production' },
         // wait_ready    : true,
         // watch: ["build"],
-        instances: 3,          // number of workers you want to run
+        instances: 2,          // number of workers you want to run
         // "env": {
         //     "PORT": "9090" // the port on which the app should listen
         // }
