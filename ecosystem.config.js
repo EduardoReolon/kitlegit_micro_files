0f2a4fc6104345c2b882872dff719e6c7a3dc814
EduardoReolon <eduardo_reolon@hotmail.com>
2023-04-24 17:23:31 -0300

    ecossystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,7 +4,7 @@ module.exports = {
         script: "build/index.js",// name of the startup file
         exec_mode: "cluster",  // to turn on cluster mode; defaults to 'fork' mode
         max_memory_restart: "1900M",
-        kill_timeout : 30000, // 30 segundos
+        kill_timeout : 300000, // 300 segundos
         // env           : { 'LD_PRELOAD': '/usr/lib/x86_64-linux-gnu/libjemalloc.so.1', 'NODE_ENV': 'production' },
         // env_production: { 'LD_PRELOAD': '/usr/lib/x86_64-linux-gnu/libjemalloc.so.1', 'NODE_ENV': 'production' },
         // wait_ready    : true,
