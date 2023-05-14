39df4e1112e157a8e7c246fea7e221699c6d5a94
EduardoReolon <eduardo_reolon@hotmail.com>
2023-05-14 09:11:49 -0300

    python implementation

--- python/storage.py
+++ python/storage.py
@@ -4,15 +4,11 @@ import cv2
 
 
 def downloadAsCv2(params: dict[str, str]):
-    print(1)
     blob_service_client = BlobServiceClient.from_connection_string(
         'DefaultEndpointsProtocol=https;AccountName={};AccountKey={};EndpointSuffix=core.windows.net'
         .format(params['account'], params['account_key']))
-    print(2)
     container_client = blob_service_client.get_container_client(
         container=params['container'])
-    print(3)
     nparr = np.frombuffer(container_client.download_blob(
         params['relPath']).readall(), np.uint8)
-    print(4)
     return cv2.imdecode(nparr, cv2.IMREAD_COLOR)
